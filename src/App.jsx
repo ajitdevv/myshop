a2508645f850e6d5d7612bf165e94b92d0bf361e
ajitdevv <ajitdev014@gmail.com>
2025-12-18 19:26:26 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useDispatch } from "react-redux";
 import Header from "./components/header.jsx";
 import Product from "./components/Product.jsx";
 import { removeallitems } from "./redux/slice.js";
-function App() {
+ function App() {
   const dispatch = useDispatch();
   return (
     <>
       <Header />
       <div className="w-full h-full flex justify-center items-start bg-gray-800">
-        <button onClick={()=>dispatch(removeallitems(0))} className="bg-black mt-15  text-white px-4 py-2 rounded">
+        <button onClick={()=>dispatch(removeallitems(0))} className="bg-gray-900  hover:bg-gray-950 mt-15  text-white px-4 py-2 rounded">
           clear cart
         </button>
       </div>
--- src/components/clearbtn.jsx
+++ src/components/clearbtn.jsx
@@ -1,7 +1,8 @@
 function Clearbtn() {
     return (
         <section className="w-full h-full flex justify-center items-start bg-gray-800">
-            <button className="bg-black mt-15  text-white px-4 py-2 rounded">clear cart</button>
+            <button className="bg-gray-900 hover:bg-gray-950
+             mt-15  text-white px-4 py-2 rounded">clear cart</button>
         </section>
     );
 }
