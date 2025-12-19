947a443174ad3f627b991ecde6bc2bf52b5241b5
ajitdevv <ajitdev014@gmail.com>
2025-12-19 22:11:12 +0530

    commit

--- src/components/Cartitem.jsx
+++ src/components/Cartitem.jsx
@@ -5,13 +5,13 @@ function Cartitem() {
   console.log(cartSlector);
   return (
     <section id="cart" className="w-full h-full bg-gray-800 text-white ">
-      <div className="container w-full mt-20">
+      <div className="container flex flex-row justify-center items-center w-full mt-20">
         {cartSlector.length > 0 ? (
-          <div className="grid grid-cols-1 gap-6 mt-20 p-6">
+          <div className="grid grid-cols-1 w-full gap-6 mt-20 p-6">
             {cartSlector.map((item) => (
               <div
                 key={item.id}
-                className="bg-white flex p-4 h-50 w-full items-center rounded-lg shadow-md text-black"
+                className="bg-white flex justify-between p-4 h-50 w-full items-center rounded-lg shadow-md text-black"
               >
                 <div className="">
                   <img className="size-40" src={item.thumbnail} alt={item.title} />
@@ -24,6 +24,9 @@ function Cartitem() {
             ))}
           </div>
         ) : null}
+      </div>
+      <div className="text-bold bg-green-300 flex items-start">
+        Total : {cartSlector.reduce((sum,item)=>sum+item.price,0)}
       </div>
     </section>
   );
