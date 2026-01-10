d318a0b90ec7c7f132b3e201b6f440a6868f2dd1
ajitdevv <ajitdev014@gmail.com>
2026-01-10 21:42:38 +0530

    order Form his done

--- src/Componenets/OrderForm.jsx
+++ src/Componenets/OrderForm.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 const OrderForm = () => {
     const dispatch = useDispatch();
     const Navigate = useNavigate();
-    const RemoveAllItems =()=> {
+    const OrderSuccess =()=> {
         localStorage.clear();
         dispatch(removeallitems());
         Navigate("/products");
@@ -16,45 +16,45 @@ const OrderForm = () => {
       <form action="" className="flex flex-col w-full gap-4 ">
         <div className="flex gap-5 ">
           <input
-            className="bg-inner-card w-[50%] p-2"
+            className="bg-inner-card rounded-xl w-[50%] p-2"
             type="text"
             required
-            placeholder="Enter your First Name"
+            placeholder="First Name"
           />
           <input
-            className="bg-inner-card w-[45%] p-2"
+            className="bg-inner-card rounded-xl w-[45%] p-2"
             type="text"
-            placeholder="Enter your Last Name"
+            placeholder="Last Name"
           />
         </div>
         <input
-          className="bg-inner-card p-2"
+          className="bg-inner-card rounded-xl p-2"
           type="text"
           required
           placeholder="Enter your address"
         />
         <div className="flex gap-5">
           <input
-            className="bg-inner-card w-[50%] p-2"
+            className="bg-inner-card rounded-xl w-[50%] p-2"
             type="number"
             required
             placeholder="Enter your number"
           />
           <input
-            className="bg-inner-card w-[45%] p-2"
+            className="bg-inner-card rounded-xl w-[45%] p-2"
             type="number"
             required
             placeholder="Pincode"
           />
         </div>
-        <select required name="payment type" id="pay" className="bg-inner-card p-2">
+        <select required name="payment type" id="pay" className="bg-inner-card rounded-xl p-2">
           <option disabled>Select Payment Method</option>
           <option value="cash">Cash On Delivery</option>
           <option value="online">Online Payment</option>
           <option value="Credit card">Credit Card</option>
           <option value="Debit card">Debit Card</option>
         </select>
-        <ButtonN onClick={RemoveAllItems} text="Place Order" />
+        <ButtonN onClick={OrderSuccess} text="Place Order" />
       </form>
     </div>
   );
