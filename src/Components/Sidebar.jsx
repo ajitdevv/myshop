3fc566240e52a3d3bb25729b0b298444b107cbef
ajitdevv <ajitdev014@gmail.com>
2026-01-19 20:14:38 +0530

    Add humber menu

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -2,7 +2,7 @@ import SidemanuButton from "../SideManulayouts/Sidemanubutton.jsx";
 import { useState } from "react";
 import SideManuContent from "../SideManulayouts/manucontent.jsx";
 const Sidemanu = () => {
-  const [isopen, setisopen] = useState(true);
+  const [isopen, setisopen] = useState(false);
   return (
     <section className="z-25 absolute right-0 top-0">
       <div className="">
