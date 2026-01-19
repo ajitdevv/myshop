69d7906671b7835ccb35aa45da55aa1188a9ceb9
ajitdevv <ajitdev014@gmail.com>
2026-01-19 20:47:28 +0530

    ...

--- src/SideManulayouts/manucontent.jsx
+++ src/SideManulayouts/manucontent.jsx
@@ -1,9 +1,14 @@
 import { ArrowDown } from "lucide-react";
 import { ButtonN } from "../Components/Button";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 const SideManuContent = ({ setisopen, open }) => {
   const [Category, setCategory] = useState(false);
+  useEffect(() => {
+    if (!open) {
+      setCategory(false);
+    }
+  }, [open]);
   return (
     <aside
       className={`
@@ -12,7 +17,7 @@ const SideManuContent = ({ setisopen, open }) => {
           ${open ? "translate-x-0" : "translate-x-full"}
         `}
     >
-      <div className="flex flex-col justify-center items-center">
+      <div className="flex flex-col justify-center items-center mt-20">
         <nav className="flex flex-col gap-4 p-6 font-bold">
           <Link to="/" onClick={() => setisopen(false)}>
             Home
@@ -22,28 +27,54 @@ const SideManuContent = ({ setisopen, open }) => {
             Products
           </Link>
           <Link>
-            <div onClick={()=>setCategory((prev) => !prev)} className="flex gap-1">
+            <div
+              onClick={() => setCategory((prev) => !prev)}
+              className="flex gap-1"
+            >
               Category Products <ArrowDown />
             </div>
           </Link>
           {Category ? (
             <div className="flex flex-col gap-3">
-              <Link to="/products/Category/beauty" onClick={() => setisopen(false)}>
+              <Link
+                to="/products/Category/beauty"
+                onClick={() => setisopen(false)}
+              >
                 Beauty
               </Link>
 
-              <Link to="/products/Category/fragrances" onClick={() => setisopen(false)}>
+              <Link
+                to="/products/Category/fragrances"
+                onClick={() => setisopen(false)}
+              >
                 Fragrances
               </Link>
-              <Link to="products/Category/groceries" onClick={() => setisopen(false)}>
+              <Link
+                to="products/Category/groceries"
+                onClick={() => setisopen(false)}
+              >
                 Groceries
               </Link>
 
-              <Link to="/products/Category/furniture" onClick={() => setisopen(false)}>
+              <Link
+                to="/products/Category/furniture"
+                onClick={() => setisopen(false)}
+              >
                 Furniture
               </Link>
             </div>
-          ) : ''}
+          ) : (
+            ""
+          )}
+          <Link to="/cart" onClick={() => setisopen(false)}>
+            Cart
+          </Link>
+          <Link to="/about" onClick={() => setisopen(false)}>
+            About
+          </Link>
+          <Link to="/trem&conditions" onClick={() => setisopen(false)}>
+            Trem&conditions
+          </Link>
         </nav>
         <div className="p-6">
           <ButtonN text="Login" />
